c44a5b98de8fdae0e5cdac4cbcb3893ea9024dd8
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-16 18:03:34 +0100

    Added script to remove windows spotlight persistent desktop icon.

---
+++ windows-configuration/scripts-pack/windows-background-spotlight-remove-desktop-icon.bat
@@ -0,0 +1,10 @@
+:: Set title and banner ::
+call set-title-and-banner "Windows Spotlight - Remove desktop icon"
+
+:: Registry folder paths ::
+set desktopPath="HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
+
+:: Changes registry value to remove "More about this picture" desktop icon ::
+reg add %desktopPath% /v {2cc5ca98-6485-489a-920e-b3e88a6ccce3} /t REG_DWORD /d 1 /f
+
+:: No restart required ::
