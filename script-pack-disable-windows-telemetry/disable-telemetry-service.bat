11559651e2391af3638d6377a425785c07d8e366
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-17 21:44:34 +0100

    cleaner implementation: changed instruction order to exit woth success code

--- script-pack-disable-windows-telemetry/disable-telemetry-service.bat
+++ script-pack-disable-windows-telemetry/disable-telemetry-service.bat
@@ -1,11 +1,11 @@
 :: Vars ::
 set serviceName=DiagTrack
 
-:: Disable auto startup ::
-sc config %serviceName% start=demand > nul
-
 :: Stop service ::
 sc stop %serviceName% > nul 2>nul
+
+:: Disable auto startup ::
+sc config %serviceName% start=demand > nul
 
 :: Restart required ::
 call %utilsPath%\restart-manager
--- script-utils/check-error-level.bat
+++ script-utils/check-error-level.bat
@@ -3,7 +3,7 @@
 :: -1978335212 - package not found (already uninstalled)
 
 :: Check if error level is not any of the OK values ::
-if not %errorlevel%==0 if not %errorlevel%==-1978335189 if not %errorlevel%==-1978335212 if not %errorlevel%==1062 (
+if not %errorlevel%==0 if not %errorlevel%==-1978335189 if not %errorlevel%==-1978335212 (
 	color 0c
 	set allScriptsOk=0
 ) else (
