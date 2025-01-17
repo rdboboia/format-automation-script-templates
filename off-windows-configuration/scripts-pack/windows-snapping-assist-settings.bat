6616dffd9ea8a6dde0fdd633ba35af0d50271e43
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-17 15:13:59 +0100

    added script to disable snapping assist (general snapping still on)

---
+++ off-windows-configuration/scripts-pack/windows-snapping-assist-settings.bat
@@ -0,0 +1,11 @@
+:: Sets the title of the window ::
+title Windows snapping assist settings
+
+:: Registry folder paths ::
+set snapAssistPath="HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+
+:: Show file extensions
+reg add %snapAssistPath% /v SnapAssist /t REG_DWORD /d 0 /f
+
+:: Requires restart ::
+set restartRequired=1
