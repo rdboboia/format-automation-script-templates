468afa58beca02afdd783e0a21753f3af92bc9a4
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-21 17:15:21 +0100

    Added script to enable numlock at startup

---
+++ script-pack-windows-configuration/configure-keyboard-numlock-on-by-default_w10_w11.bat
@@ -0,0 +1,10 @@
+:: Enables numlock ON by default (useful for Hyper-V VMs) ::
+
+:: Set title and banner ::
+call %utilsPath%\set-title-and-banner "Configure windows admin elevation prompt"
+
+:: Changes the registry to ask for password on admin privilege prompt ::
+reg add "HKCU\.DEFAULT\Control Panel\Keyboard" /v InitialKeyboardIndicators /t REG_SZ /d 2 /f
+
+:: Requires restart ::
+call %utilsPath%\restart-manager
