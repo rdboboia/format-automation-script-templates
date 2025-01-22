9828cbe36bf7d15e9ecea5bc556a138aa6ff6402
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-22 23:36:05 +0100

    added more error code to the non errors list

--- script-utils/check-error-level.bat
+++ script-utils/check-error-level.bat
@@ -1,10 +1,13 @@
 :: Winget error checking ::
+:: 1 and 2 for choices
+:: 3010 - reject windows feature install restart
 :: -1978335189 - already installeed and no updates available
 :: -1978335212 - package not found (already uninstalled)
 
+
 :: Check if error level is not any of the OK values ::
-if not %errorlevel%==0 if not %errorlevel%==-1978335189 if not %errorlevel%==-1978335212 (
-	color 0c
+echo %errorlevel%
+if not %errorlevel%==0 if not %errorlevel%==1 if not %errorlevel%==2 if not %errorlevel%==3010 if not %errorlevel%==-1978335189 if not %errorlevel%==-1978335212 (
 	set allScriptsOk=0
 ) else (
 	color 0a
