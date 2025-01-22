5c488811c9990ab08bcc5bc5b6f2c8361cefed5c
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-22 23:35:11 +0100

    added check to skip sandbox install on my vm

--- script-pack-manage-windows-features/enable-sandbox-feature.bat
+++ script-pack-manage-windows-features/enable-sandbox-feature.bat
@@ -1,5 +1,10 @@
+:: Check if the current system is my VM (change this variable to match your case) ::
+if "%computername%"=="HYPER-V-VM-W11" (goto end)
+
 :: Enable Hyper-V feature ::
 DISM /Online /Enable-Feature /All /FeatureName:Containers-DisposableClientVM
 
 :: Requires restart ::
 call %utilsPath%\restart-manager
+
+:end
