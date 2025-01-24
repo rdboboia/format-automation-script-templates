195d7e4ae4ec3b15854916c4938dbd7d0445f473
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-24 16:21:32 +0100

    minor fixes

--- script-pack-windows-configuration/optional-disable-pagefile_w10_w11.bat
+++ script-pack-windows-configuration/optional-disable-pagefile_w10_w11.bat
@@ -3,5 +3,5 @@ choice /c yn
 
 :: Call powershell script to disable and delete pagefile ::
 if %errorlevel%==1 (
-	powershell -noexit -ExecutionPolicy Bypass -file "disable-and-delete-pagefile-powershell-script.ps1"
+	powershell -ExecutionPolicy Bypass -file "%packExecutorArgs%\disable-and-delete-pagefile-powershell-script.ps1"
 )
