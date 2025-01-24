46536cf83a64e6ef0f054579b24c13fdd35a4073
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-24 15:31:09 +0100

    added power saver power plan configuration

---
+++ script-pack-windows-configuration/configure-power-plan-power-saver_w10_w11.bat
@@ -0,0 +1,30 @@
+:: Power Saver power plan configuration ::
+:: It is intended to be used for desktops with SSDs ::
+:: This configuration should enable better power management while also improving responsiveness ::
+
+:: Set title and banner ::
+call %utilsPath%\set-title-and-banner "Configure power saver power plan"
+
+:: Set the plan to be modified (BALANCED power plan GUID: a1841308-3541-4fab-bc81-f71556f20b4a) ::
+set powerPlanGuid=a1841308-3541-4fab-bc81-f71556f20b4a
+
+:: Disable idle disk shutdown ::
+powercfg /setacvalueindex %powerPlanGuid% SUB_DISK DISKIDLE 0
+REM powercfg /setdcvalueindex %powerPlanGuid% SUB_DISK DISKIDLE 0
+
+:: Disable sleep due to inactivity ::
+powercfg /setacvalueindex %powerPlanGuid% SUB_SLEEP STANDBYIDLE 0
+REM powercfg /setdcvalueindex %powerPlanGuid% SUB_SLEEP STANDBYIDLE 0
+
+:: Limit the max CPU usage (may vary from system to system) ::
+powercfg /setacvalueindex %powerPlanGuid% SUB_PROCESSOR PROCTHROTTLEMAX 95
+REM powercfg /setdcvalueindex %powerPlanGuid% SUB_PROCESSOR PROCTHROTTLEMAX 95
+
+:: Disable screen shutdown ::
+powercfg /setacvalueindex %powerPlanGuid% SUB_VIDEO VIDEOIDLE 0
+REM powercfg /setdcvalueindex %powerPlanGuid% SUB_VIDEO VIDEOIDLE 0
+
+:: Disable USB energy savings (useful when a KVM is used to get faster peripherals switch) ::
+:: Might be needed; leaving it off for now until proper tests are performed in w11
+
+:: No restart required ::
