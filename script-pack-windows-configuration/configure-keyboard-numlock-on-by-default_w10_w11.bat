a5b1bcb8e836d764165457459f8f95a9acc7d85a
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-31 20:30:30 +0100

    Update configure-keyboard-numlock-on-by-default_w10_w11.bat

--- script-pack-windows-configuration/configure-keyboard-numlock-on-by-default_w10_w11.bat
+++ script-pack-windows-configuration/configure-keyboard-numlock-on-by-default_w10_w11.bat
@@ -5,6 +5,7 @@ call %utilsPath%\set-title-and-banner "Configure windows admin elevation prompt"
 
 :: Changes the registry to ask for password on admin privilege prompt ::
 reg add "HKCU\.DEFAULT\Control Panel\Keyboard" /v InitialKeyboardIndicators /t REG_SZ /d 2 /f
+:: reg add "HKCU\Control Panel\Keyboard" /v InitialKeyboardIndicators /t REG_SZ /d 2 /f
 
 :: Requires restart ::
 call %utilsPath%\restart-manager
