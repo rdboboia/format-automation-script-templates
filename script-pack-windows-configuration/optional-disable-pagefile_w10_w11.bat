0261c5d6c6980a10cd4632891652b47bcda52915
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-31 20:31:08 +0100

    modified script to be able to be executed as a standalone

--- script-pack-windows-configuration/optional-disable-pagefile_w10_w11.bat
+++ script-pack-windows-configuration/optional-disable-pagefile_w10_w11.bat
@@ -3,5 +3,9 @@ choice /c yn
 
 :: Call powershell script to disable and delete pagefile ::
 if %errorlevel%==1 (
-	powershell -ExecutionPolicy Bypass -file "%packExecutorArgs%\disable-and-delete-pagefile-powershell-script.ps1"
+	if "%packExecutorArgs%"=="" (
+		powershell -ExecutionPolicy Bypass -file "disable-and-delete-pagefile-powershell-script.ps1"
+	) else (
+		powershell -ExecutionPolicy Bypass -file "%packExecutorArgs%\disable-and-delete-pagefile-powershell-script.ps1"
+	)
 )
