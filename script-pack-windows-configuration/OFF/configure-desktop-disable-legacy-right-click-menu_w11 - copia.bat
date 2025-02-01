9e7026639c525b4903aea2fa2fddc77d89a007c4
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-02-01 12:05:17 +0100

    added scripts to enable and disable legacy right click menu by default

---
+++ script-pack-windows-configuration/OFF/configure-desktop-disable-legacy-right-click-menu_w11 - copia.bat
@@ -0,0 +1,8 @@
+:: Set title and banner ::
+call %utilsPath%\set-title-and-banner "Windows Desktop - Disable legacy right click menu"
+
+:: Changes registry value ::
+reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
+
+:: Requires restart ::
+call %utilsPath%\restart-manager
