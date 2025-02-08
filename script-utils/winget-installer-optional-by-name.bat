5510018704e29363c046fde7a69ae12e2abd2799
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-02-08 23:45:37 +0100

    working on optional installer based on script name (easier to manage)

---
+++ script-utils/winget-installer-optional-by-name.bat
@@ -0,0 +1,25 @@
+:: Set title and banner.
+call %utilsPath%\set-title-and-banner "%~1"
+
+:: Load args.
+set scriptName=%~3
+
+:: Check if is an optional script.
+if not %scriptName:~0,8%==optional (
+	goto install
+)
+
+:: Check if install is desired ::
+choice /c yn
+
+:: Check decision ::
+if not %errorlevel%==1 (
+	echo Installation skipped.
+	goto end
+)
+
+:: Install ::
+:install
+winget install --exact --id "%~2"
+
+:end
