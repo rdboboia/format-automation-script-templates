be64ef02ef4334fe6ee29cb55742655ddc2686db
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-02-08 19:56:38 +0100

    made print filler more customizable

--- script-utils/print-filler.bat
+++ script-utils/print-filler.bat
@@ -1,11 +1,32 @@
+:: Load args.
 set printerIterations=%~1
+set printerChar=%~2
+set lineLength=%~3
 
-:loop
+:: Initialize variables (if required).
+set lineContent=
+
+if "%printerChar%"=="" (
+	set printerChar=*
+)
+
+if "%lineLength%"=="" (
+	set lineLength=100
+)
+
+:: Create line content.
+:lineLoop
+set /a lineLength=%lineLength% - 1
+set lineContent=%lineContent%%printerChar%
+if not %lineLength% LEQ 0 (goto lineLoop)
+
+:: Print line content the requested number of times (default is 1).
+:rowsLoop
 :: Reduce iterations left ::
 set /a printerIterations=%printerIterations% - 1
 
 :: Print spacer ::
-echo **********************************************************************************************
+echo %lineContent%
 
 :: Repeat while iterations > 0 ::
-if not %printerIterations% LEQ 0 (goto loop)
+if not %printerIterations% LEQ 0 (goto rowsLoop)
